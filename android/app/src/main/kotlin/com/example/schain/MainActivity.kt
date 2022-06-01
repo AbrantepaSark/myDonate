2af68392ae0e272cae2973e7f22c71350e9a83a3
Sark <dsarkodie500@gmail.com>
2022-06-01 15:27:01 -0700

    Creating Schain

---
+++ android/app/src/main/kotlin/com/example/schain/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.schain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/Splash_Screen.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: const [
+          Positioned.fill(
+            child: Image(
+                image: AssetImage("assets/images/background.png"),
+                fit: BoxFit.fill),
+          )
+        ],
+      ),
+    );
+  }
+}
